1e9c070502b5fbd475653c6619c1187d7c2e40b2
Ricardo <ricardosem@gmail.com>
2021-12-14 17:45:08 +0000

    Atualizando os dados do site.

---
+++ R/passoapasso.R
@@ -0,0 +1,32 @@
+#passo 2:
+  usethis::create_package("blog")
+
+#passo 4:
+  usethis::use_git()
+
+#passo 5:
+  usethis::use_github()
+
+#passo 6:
+  usethis::use_github_pages("master", "/docs")
+
+#passo 7:
+  file.create(".nojekyll")
+
+#passo 7.1:
+  Sys.setenv(TZ='GMT')
+
+#passo 8:
+  distill::create_blog(".", "meu_blog", gh_pages = TRUE)
+
+#passo 9:
+    distill::create_post("Título do Meu Primeiro Post")
+
+#passo 10:
+    commit e push
+
+#passo 11:
+    acessar [seu_login].github.io/[seu_repositorio]
+
+    distill::create_post("Análise da Base RAIS")
+
