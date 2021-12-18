7f7ded79535adf2fbef2d788ae4b24f76101ee7f
Ricardo <ricardosem@gmail.com>
2021-12-18 18:38:00 +0000

    Atualizando - Acessando banco de dados!

--- R/passoapasso.R
+++ R/passoapasso.R
@@ -1,32 +1,27 @@
+#passo 1:
+install.packages(c("usethis", "distill"))
 #passo 2:
-  usethis::create_package("blog")
-
+usethis::create_package("blog")
+#passo 3:
+usethis::use_git_config(
+  user.name = "Fernando",
+  user.email = "fptcorrea@gmail.com")
+#passo 3.5:
+usethis::create_github_token()
+#passo 3.75:
+gitcreds::gitcreds_set()
 #passo 4:
-  usethis::use_git()
-
+usethis::use_git()
+#sair do projeto e voltar
 #passo 5:
-  usethis::use_github()
-
+usethis::use_github()
 #passo 6:
-  usethis::use_github_pages("master", "/docs")
-
+usethis::use_github_pages("master", "/docs")
 #passo 7:
-  file.create(".nojekyll")
-
-#passo 7.1:
-  Sys.setenv(TZ='GMT')
-
+file.create(".nojekyll")
+#passo 7.1: meu note
+Sys.setenv(TZ='GMT')
 #passo 8:
-  distill::create_blog(".", "meu_blog", gh_pages = TRUE)
-
-#passo 9: criar post novo
-    distill::create_post("Título do Meu Primeiro Post")
-
-#passo 10:
-    commit e push
-
-#passo 11:
-    acessar [seu_login].github.io/[seu_repositorio]
-
-    distill::create_post("Análise da Base RAIS")
-
+distill::create_blog(".", "meu_blog", gh_pages = TRUE)
+#passo 9:
+distill::create_post("Título do Meu Primeiro Post")
